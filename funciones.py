b36bcad29b8e2abc0006defd0aa935046088ef24
paloma <padosa23@gmail.com>
2018-10-05 12:29:45 +0200

    Mi app Django Girls, primer commit

---
+++ funciones.py
@@ -0,0 +1,11 @@
+def hi(name):
+    if name == 'Paloma':
+        print('Hi Paloma!')
+    elif name == 'Elena':
+        print('Hi Elena!')
+    else:
+        print('Hi Andros!')
+
+
+for i in range(1, 6):
+    print(i)
---
+++ prueba1.py
@@ -0,0 +1,8 @@
+name = 'Paloma'
+name2 = 'Elena'
+if name == 'Paloma':
+    print('Hey Paloma!')
+elif name2 == 'Elena':
+    print('Hey Elena!')
+else:
+    print('Hey anonymous!')
